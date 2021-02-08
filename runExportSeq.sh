6238581da443b9cb26eadbed47e5eab42dc8d280
Marcos Garanito <mpgaranito@gmail.com>
2021-02-08 11:29:16 -0300

    Shell Script - Initial

---
+++ runExportSeq.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DATE_UNIX_ERA=$(date -u +%s)
+echo $DATE_UNIX_ERA
+echo $(date -u --date=@$DATE_UNIX_ERA)
+INICIAL_DAY=$(date -u --date=@$DATE_UNIX_ERA +%0d)
+FINAL_DAY=$INICIAL_DAY
+INICIAL_MONTH=$(date -u --date=@$DATE_UNIX_ERA +%m)
+INICIAL_YEAR=$(date -u --date=@$DATE_UNIX_ERA +%Y)
+INICIAL_HOUR=$(date -u --date=@$DATE_UNIX_ERA +%H)
+INICIAL_MINUTE=$(date -u --date=@$DATE_UNIX_ERA +%M)
+echo $INICIAL_HOUR
+if [ $INICIAL_HOUR -gt 0 ]; then
+    INICIAL_HOUR=$(expr "$INICIAL_HOUR" - "1")
+else
+    INICIAL_HOUR=23
+    INICIAL_DAY=$(expr "$INICIAL_DAY" - "1")
+    if [ $INICIAL_DAY -lt 10 ]; then
+        INICIAL_DAY=$(echo '0'$INICIAL_DAY)
+    fi
+fi
+if [ $INICIAL_HOUR -lt 10 ]; then
+    echo $INICIAL_HOUR
+    INICIAL_HOUR=$(echo '0'$INICIAL_HOUR)
+    echo $INICIAL_HOUR
+fi
+cd /mnt/d/take/ArquivosAnaliseLog/seqcli-2020.4.365-linux-x64/
+./seqcli query -q="select * from stream where Application = 'claro-faturamento' and  @Level = 'Debug' limit 100000" --start="$INICIAL_YEAR-$INICIAL_MONTH-$INICIAL_DAY"T"$INICIAL_HOUR:00:00.000Z" --end="$INICIAL_YEAR-$INICIAL_MONTH-$FINAL_DAY"T"$INICIAL_HOUR:59:59.000Z" >files_$INICIAL_YEAR-$INICIAL_MONTH-$FINAL_DAY-$INICIAL_HOUR.txt
+# wput files_$INICIAL_YEAR-$INICIAL_MONTH-$FINAL_DAY-$INICIAL_HOUR.txt ftp://root:dev123@192.168.0.1/sda2/take/logs/files_$INICIAL_YEAR-$INICIAL_MONTH-$FINAL_DAY-$INICIAL_HOUR.txt
